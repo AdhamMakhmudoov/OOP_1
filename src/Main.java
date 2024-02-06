d4ecdf2c58d5640c3f6abc9defc0417c77325661
AdhamMakhmudoov <AdhamMakhmudov1996@gmail.com>
2024-02-06 14:35:07 +0500

    OOP_1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String... args){
+
+            Person person = new Person("Adham","Yunsobod 19");
+            System.out.println(person);
+
+            System.out.println();
+
+            Student student = new Student("Adham","Yunsobod 19","Dasturlash",26,45d);
+            System.out.println(student);
+
+            System.out.println();
+
+            Staff staff = new Staff("Bobur","Chilonzor 5","258 - maktab",860000);
+            System.out.println(staff);
+    }
+}
